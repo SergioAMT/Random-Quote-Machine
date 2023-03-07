f0687d6d6db9b04d2003b1352d004387f831b4df
Sergio <alejandrosamt@gmail.com>
2023-03-06 22:41:15 -0400

    agregando test de FCC

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,7 +2,7 @@ export const Buttons = (props) => {
     return(
         <div className="container">
             <div className="socialMedia" >
-            <a class="button" id="tweet-quote" href="https://twitter.com/?lang=es"><i class="bi bi-twitter"></i></a>
+            <a class="button" id="tweet-quote" href="twitter.com/intent/tweet"><i class="bi bi-twitter"></i></a>
             <a class="button" href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>
             </div>
             
